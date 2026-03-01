64c8bae232df8c5d9f8b188b95a1beee99d9b88b
Sniffaa <d9006966@gmail.com>
2026-02-28 23:38:00 -0600

    added list btn & task name field & submit btn

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,19 @@ function App() {
       <div className='Header'>
         <div className='Logo'>List & Do</div>
       </div>
+        <p>To add a new task, type your task into the input box, then click the Submit button. Your task will then be added to the list you selected.</p>
+      <div className='Input'>
+        <input className='HighRadio' type='radio' title='High Priority' name='Option'></input>
+        <input className='MediumRadio' type='radio' title='Medium Priority' name='Option'></input>
+        <input className='LowRadio' type='radio' title='Low Priority' name='Option'></input>
+        <input className='Box'></input>
+        <input className='Add' type='submit'></input>
+      </div>
+        <hr></hr>
       <div className='Container'>
-        <div className='High'>🔴 High Priority 🔴<hr></hr></div>
-        <div className='Medium'>🟡 Medium Priority 🟡<hr></hr></div>
-        <div className='Low'>🟢 Low Priority 🟢<hr></hr></div>
+        <div className='HighList'>🔴 High Priority 🔴<hr></hr></div>
+        <div className='MediumList'>🟡 Medium Priority 🟡<hr></hr></div>
+        <div className='LowList'>🟢 Low Priority 🟢<hr></hr></div>
       </div>
       <div className='Footer'>© 2026 List & Do</div>
     </div>
