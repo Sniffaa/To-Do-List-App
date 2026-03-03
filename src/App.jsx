0305e58d4275ba9d17d44aa09dbc6d8cbdfa82b4
Sniffaa <d9006966@gmail.com>
2026-03-03 00:47:51 -0600

    can now create task

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,41 @@ import './App.css'
 
 function App() {
 
+  const [taskInput, setInput] = useState('');
+  const [taskPriority, setPriority] = useState('');
+  const [task, setTasks] = useState({ high: [], medium: [], low: [] });
+
+  // Grabs What List To Store Task In And Adds The Users Text Into The Selected List
+  function createTask() {setTasks(prev => ({...prev,[taskPriority]: [...prev[taskPriority],taskInput]}));
+  // Resets Fields Back To Empty
+  setInput('');
+  setPriority('');
+  }
+
   return (
     <div className='App'>
       <div className='Header'>
         <div className='Logo'>List & Do</div>
       </div>
-        <p>To add a new task, type your task into the input box, then click the Submit button. Your task will then be added to the list you selected.</p>
-      <div className='Input'>
-        <input className='HighRadio' type='radio' title='High Priority' name='Option'></input>
-        <input className='MediumRadio' type='radio' title='Medium Priority' name='Option'></input>
-        <input className='LowRadio' type='radio' title='Low Priority' name='Option'></input>
-        <input className='Box'></input>
-        <input className='Add' type='submit'></input>
+        <p>To add a new task, type your task into the input box, then click the Submit button.</p>
+      <div className='Form'>
+        <input className='HighRadio'type='radio' value='high' name='Option' checked={taskPriority === 'high'} onChange={(e) => setPriority(e.target.value)}/>
+        <input className='MediumRadio' type='radio' value='medium' name='Option' checked={taskPriority === 'medium'} onChange={(e) => setPriority(e.target.value)}/>
+        <input className='LowRadio' type='radio' value='low' name='Option' checked={taskPriority === 'low'} onChange={(e) => setPriority(e.target.value)}/>
+        <input className='Box' value={taskInput} onChange={(e) => setInput(e.target.value)}/>
+        <button className='Add' onClick={createTask}>Add</button>
       </div>
-        <hr></hr>
-      <div className='Container'>
-        <div className='HighList'>🔴 High Priority 🔴<hr></hr></div>
-        <div className='MediumList'>🟡 Medium Priority 🟡<hr></hr></div>
-        <div className='LowList'>🟢 Low Priority 🟢<hr></hr></div>
+        <hr/>
+      <div className='Lists'>
+        <div className='HighList'>🔴 High Priority 🔴<hr/>
+        {task.high.map((task, index) => (<div key={index}>{task}</div>))}
+        </div>
+        <div className='MediumList'>🟡 Medium Priority 🟡<hr/>
+          {task.medium.map((task, index) => (<div key={index}>{task}</div>))}
+        </div>
+        <div className='LowList'>🟢 Low Priority 🟢<hr/>
+        {task.low.map((task, index) => (<div key={index}>{task}</div>))}
+        </div>
       </div>
       <div className='Footer'>© 2026 List & Do</div>
     </div>
